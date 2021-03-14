a728a188edd62c4fcff156618624a041898ed670
Abdelrahman Tareq <abdotarekfathy@gmail.com>
2021-03-14 21:21:14 +0200

    Creating & Managing a Dummy User

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const { use } = require('./routes/admin');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -23,13 +24,32 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
+
+
 app.use(errorController.get404);
+app.use((req, res, next) => {
+    User.findByPk(1).then(user => {
+        req.user = user;
+        next();
+    }).catch(err => console.log(err));
+});
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
 
 // this to map models to tables
-sequelize.sync({ force: true }).then(result => {
-    app.listen(3000);
-}).catch(err => console.log(err))
+sequelize.sync().then(result => User.findByPk(1)).then(user => {
+    if (!user) {
+        const temp = { name: 'Abdo', email: 'abc@mail.com' };
+        console.log('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', temp);
+        return User.create(temp);
+    }
+    return user;
+}
+).then(
+    user => {
+        console.log(user);
+        app.listen(3000);
+    }
+).catch(err => console.log(err))
 
