668f50461822233277c5b2774e8c48965bd09dd3
Abdelrahman Tareq <abdotarekfathy@gmail.com>
2021-03-17 07:36:35 +0200

    Creating New Users

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const mongoConnect = require('./util/database').mongoConnect;
 
+const User = require('./models/user');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -18,10 +20,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    // User.findByPk(1).then(user => {
-    //     req.user = user;
-    next();
-    // }).catch(err => console.log(err));
+    User.findById('6051878c6265402a2000860f').then(user => {
+        req.user = user;
+        next();
+    }).catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
@@ -29,4 +31,6 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => app.listen(3000));
+mongoConnect(() => {
+    app.listen(3000);
+});
--- models/user.js
+++ models/user.js
@@ -1,15 +1,29 @@
-const Sequelize = require('sequelize');
-const sequelize = require('../util/database');
+const mongoDb = require('mongodb');
+const getDb = require('../util/database').getDb;
 
-const User = sequelize.define('user', {
-    id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
-    },
-    name: Sequelize.STRING,
-    email: Sequelize.STRING,
-});
+class User {
+    constructor(name, email, id) {
+        this.name = name;
+        this.email = email;
+        this._id = id ? new mongoDb.ObjectId(id) : null;
+    }
 
+    save() {
+        const db = getDb();
+        return db.collection('users').insertOne(this);
+    }
+
+    static findById(id) {
+        const db = getDb();
+        return db.collection('users').findOne({ _id: new mongoDb.ObjectId(id) });
+    }
+
+    static deleteById(id) {
+        const db = getDb();
+        return db.collection('users').deleteOne({ _id: new mongoDb.ObjectId(id) }).then(result => {
+            console.log(result);
+        }).catch(err => console.log(err));;
+    }
+
+}
 module.exports = User;
