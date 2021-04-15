28fbb6b14db8adc12ddded61a8a25f6ea498a0fb
Abdelrahman Tareq <abdotarekfathy@gmail.com>
2021-04-15 07:28:40 +0200

    More Validators

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { check } = require('express-validator');
+const { check, body } = require('express-validator');
 
 const authController = require('../controllers/auth');
 
@@ -11,7 +11,15 @@ router.get('/signup', authController.getSignup);
 
 router.post('/login', authController.postLogin);
 
-router.post('/signup', check('email').isEmail().withMessage('Please enter a valid email'), authController.postSignup);
+router.post('/signup', [
+    check('email').isEmail().withMessage('Please enter a valid email').custom((value, { req }) => {
+        if (value === 'abdotarekfathy@gmail.com') {
+            throw Error('personal mail');
+        }
+        return true;
+    }),
+    body('password', 'Ente valid password').isLength({ min: 5 }).isAlphanumeric()
+], authController.postSignup);
 
 router.post('/logout', authController.postLogout);
 
