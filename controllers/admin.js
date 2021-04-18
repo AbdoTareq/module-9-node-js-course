fed4b2130cde8c01329a23b993dc36a52dbc3ac6
Abdelrahman Tareq <abdotarekfathy@gmail.com>
2021-04-18 11:29:46 +0200

    Using the Express.js Error Handling Middleware

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,6 +1,12 @@
 const Product = require('../models/product');
 const { validationResult } = require('express-validator');
 
+const handleErrorFun = (err, next) => {
+  const error = Error(err);
+  error.httpStatusCode = 500;
+  return next(error);
+}
+
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
@@ -34,7 +40,9 @@ exports.getEditProduct = (req, res, next) => {
       errorMessage: null,
       validationErrors: []
     });
-  }).catch(err => console.log(err));
+  }).catch(err =>
+    handleErrorFun(err, next)
+  );
 };
 
 exports.postAddProduct = (req, res, next) => {
@@ -43,7 +51,6 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   const product = new Product({ title: title, price: price, imageUrl: imageUrl, description: description, userId: req.user });
-
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     console.log(errors.array()[0].msg);
@@ -62,7 +69,9 @@ exports.postAddProduct = (req, res, next) => {
     .then(result => {
       console.log(result);
       res.redirect('/admin/products')
-    }).catch(err => console.log(err));
+    }).catch(err =>
+      handleErrorFun(err, next)
+    );
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -110,8 +119,7 @@ exports.postEditProduct = (req, res, next) => {
         res.redirect('/admin/products');
         console.log('updated', result);
       }).catch(err => {
-        console.log(err);
-        res.redirect('/500');
+        handleErrorFun(err, next);
       })
   })
 };
